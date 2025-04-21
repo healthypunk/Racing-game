cd084cdc943201ad7a5d69f339860783e6e00e2f
evaldas <petnjunas.evaldas@gmail.com>
2025-04-21 12:51:24 +0300

    Added physics

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,78 @@
-import { Canvas, useLoader, useFrame } from '@react-three/fiber';
-import { useRef } from 'react';
-import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import { Canvas, useLoader, useFrame} from '@react-three/fiber';
+import { useRef, useState, useEffect, useMemo } from 'react';
+import { KeyboardControls, OrbitControls, PerspectiveCamera, useKeyboardControls, } from '@react-three/drei';
 
 import './App.css'
 
 import { OBJLoader } from 'three/examples/jsm/Addons.js';
 import { MTLLoader } from 'three/examples/jsm/Addons.js';
 import { FBXLoader } from 'three/examples/jsm/Addons.js';
+import { Physics, RigidBody } from '@react-three/rapier';
 
 function TrackModel() {
     const result = useLoader(FBXLoader, 'track.fbx');
     return <primitive object={result} />
 }
 
+export const Controls = {
+    forward: "forward",
+    back: "back",
+    left: "left",
+    right: "right"
+}
+
 function CarModel() {
     const carRef = useRef();
     const cameraRef = useRef();
+    const apiRef = useRef();
     const materials = useLoader(MTLLoader, '/car.mtl')
     const car = useLoader(OBJLoader, '/car.obj', (loader) => {
         materials.preload();
         loader.setMaterials(materials);
     })
-    const cameraOffset = [0.7,0.3,0]
+    const cameraOffset = [-6,5,-5]
+
+    const leftPressed = useKeyboardControls((state) => state[Controls.left]);
+    const rightPressed = useKeyboardControls((state) => state[Controls.right]);
+    const forwardPressed = useKeyboardControls((state) => state[Controls.forward]);
+    const backPressed = useKeyboardControls((state) => state[Controls.back]);
+
+    // const handleMovement = () => {
+    //     if (!apiRef.current) return;
+    //     const impulse = { x: 0, y: 0, z: 0 };
+    //     if (leftPressed) impulse.x = 0.05;
+    //     if (rightPressed) impulse.x = 0.1;
+    //     if (forwardPressed) impulse.z = 0.1;
+    //     if (backPressed) impulse.z = 0.1;
+
+    //     apiRef.current.applyImpulse(impulse, true);
+    // };
+
+    const handleMovement = () => {
+
+        const impulse = { x: 0, y: 0, z: 0 };
+        const torque = { x: 0, y: 0, z: 0 };
+
+        if (leftPressed) {
+            
+            torque.x += 0.00001;
+        }
+        if (rightPressed) {
+            torque.x -= 0.00002;
+        }
+        if (forwardPressed) {
+            impulse.x -= 0.001;
+            console.log("forward pressed")
+        }
+        if (backPressed) {
+            impulse.x += 0.001;
+            torque.z += 0.00002;   
+        }
+
+        apiRef.current.applyImpulse(impulse);
+        apiRef.current.applyTorqueImpulse(torque);
+    }
+
     useFrame((state, delta) => {
         if (carRef.current && cameraRef.current) {
             const carPosition = carRef.current.position;
@@ -34,17 +85,41 @@ function CarModel() {
             
             cameraRef.current.lookAt(carPosition);
         }
+        // if (!apiRef.current || !cameraRef.current) return;
+
+        // const { x, y, z } = apiRef.current.translation();
+
+        // cameraRef.current.position.set(
+        //     x + cameraOffset[0],
+        //     y + cameraOffset[1],
+        //     z + cameraOffset[2]
+        // );
+
+        // cameraRef.current.lookAt(carRef.current.position);
+        handleMovement();
+        
     });
 
     return (
         <>
-            <primitive 
-                ref={carRef} 
-                object={car} 
-                position={[-8, 0, -3.5]} 
-                scale={0.1} 
-                rotation={[-Math.PI/2, 0, -Math.PI/2]}
-            />
+        <RigidBody 
+                ref={apiRef}
+                type="dynamic"
+                mass={1}
+                friction={0.5}
+                restitution={0.3}
+                colliders = "hull"
+            >
+                
+                <primitive
+                    ref={carRef}
+                    object={car}
+                    position={[-8, 1, -3.5]} 
+                    scale={0.1}
+                    rotation={[-Math.PI/2, 0, -Math.PI/2]}
+                />
+            </RigidBody>
+
             <PerspectiveCamera 
                 ref={cameraRef}
                 makeDefault
@@ -56,19 +131,31 @@ function CarModel() {
 }
 
 function App() {
+    const map = useMemo(
+        () => [
+            { name: Controls.forward, keys: ["ArrowUp", "KeyW"]},
+            { name: Controls.back, keys: ["ArrowDown", "KeyS"]},
+            { name: Controls.left, keys: ["ArrowLeft", "KeyA"]},
+            { name: Controls.right, keys: ["ArrowRight", "KeyD"]}
+        ])
+    return (
+        <div id="canvas-container" >
+        <Canvas  >
+            <axesHelper />
+            <OrbitControls />
+            <directionalLight position={[2,5,1]} />
+            <KeyboardControls map = { map }>
+                <Physics gravity={[0, -9.81, 0]} debug>
+                    <RigidBody type="fixed" >
+                        <TrackModel />
+                    </RigidBody>
+                    <CarModel />
+                </Physics>
+            </KeyboardControls>
+        </Canvas>
 
-  return (
-    <div id="canvas-container" >
-      <Canvas  >
-        <axesHelper />
-        <TrackModel />
-        <CarModel />
-        <OrbitControls />
-        <directionalLight position={[2,5,1]} />
-      </Canvas>
-
-    </div>
-  )
-}
+        </div>
+    )
+    }
 
 export default App
